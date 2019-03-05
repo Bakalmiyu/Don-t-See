68d8b32492f2b6a273c4b06a5c92eb8d55a2a4f1
Bakalmiyu <33348856+Bakalmiyu@users.noreply.github.com>
2019-03-05 17:06:45 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -29,8 +29,8 @@ function maze(){
 	var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     ctx.canvas.width = window.innerWidth;
-    ctx.fillStyle = "#FF0000";
     ctx.canvas.height = window.innerHeight;
+    ctx.fillStyle = "#FF0000";
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(0,0,150,75);
 }
