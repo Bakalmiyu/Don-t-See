b246105f5dacca71128d0851d85ef6e293e4da37
Hangyuli12 <48132383+Hangyuli12@users.noreply.github.com>
2019-03-05 17:29:47 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -29,11 +29,31 @@ function maze(){
 	var canvas = document.getElementById("myCanvas");
     var ctx = canvas.getContext("2d");
     ctx.canvas.width = window.innerWidth;
-    ctx.canvas.height = window.innerHeight;
-    ctx.fillStyle = "#FF0000";
-    ctx.fillStyle = "#FF0000";
-    ctx.fillRect(0,0,150,75);
+	ctx.canvas.height = window.innerHeight;
+	ctx.fillStyle = "#FF0000";   
+	ctx.beginPath();
+	
+	ctx.moveTo(1/4*window.innerWidth,1/4*window.innerHeight);//first line
+	ctx.lineTo(3/4*window.innerWidth,1/4*window.innerHeight);
+	
+	ctx.moveTo(1/4*window.innerWidth,2/4*window.innerHeight);//2nd horizaonral
+	ctx.lineTo(3/8*window.innerWidth,2/4*window.innerHeight);
+	
+	ctx.moveTo(5/8*window.innerWidth,2/4*window.innerHeight);//3rd
+	ctx.lineTo(4/4*window.innerWidth,2/4*window.innerHeight);
+	
+	ctx.moveTo(1/4*window.innerWidth,1/4*window.innerHeight);//linedown left
+	ctx.lineTo(1/4*window.innerWidth,1/2*window.innerHeight);
+	
+	ctx.moveTo(3/8*window.innerWidth,2/4*window.innerHeight);//2nd line
+	ctx.lineTo(3/8*window.innerWidth,window.innerHeight);
+	
+	ctx.moveTo(5/8*window.innerWidth,2/4*window.innerHeight);//3rd line
+	ctx.lineTo(5/8*window.innerWidth,window.innerHeight);
+	ctx.stroke();
+	
 }
+
 function main(){
     maze();
 }
