fa6197f95e16c55925f4574f7b3ba232e69aec2b
Minh Pham <mvp57@cs.drexel.edu>
2019-03-08 09:48:02 -0500

    update the somewhat works version

--- ANS.js
+++ ANS.js
@@ -12,9 +12,19 @@ function soundLevel(event) {
 	var destination = Math.sqrt(Math.pow((player.posX - event.posX),2) - Math.pow((player.posY - event.posY),2))
 	var soundDeductionCoeff = destination/originalDistance
 	var soundDeductionCoeff = Math.log(soundDeductionCoeff)/Math.log(2)
-	return originalSoundLevel - 6*soundDeductionCoeff
+	return Math.pow(10,6*soundDeductionCoeff/20)
 }
 
-function isCollide(argument) {
-	
+function isCollide(posX, posY, maze = mazefile) {
+	for (x = 0, x > mazefile.length, x++){
+		if (posX = mazefile[x]) {
+			for (var y = 0; y < mazefile[x].length; y++) {
+				if (mazefile[x][y] == posY) {
+					return True
+				}
+			}
+		}
+	}
+	//FIXME: add a more general approach to include character?
+	return False
 }
