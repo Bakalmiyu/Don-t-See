9369a2a59ed42fb24113c5f1d1b8671148e3aa11
Bakalmiyu <33348856+Bakalmiyu@users.noreply.github.com>
2019-03-08 11:26:49 -0500

    Update Main.js

--- Main.js
+++ Main.js
@@ -14,16 +14,12 @@ document.addEventListener("keydown", function(e){//Useing Keyboard to call other
 });
 
 function moveup(){
-    document.getElementById("nani").innerHTML = "up";
 }
 function movedown(){
-    document.getElementById("nani").innerHTML = "down";
 }
 function moveright(){
-    document.getElementById("nani").innerHTML = "right";
 }
 function moveleft(){
-    document.getElementById("nani").innerHTML = "left";
 }
 function maze(){	
 	var canvas = document.getElementById("myCanvas");
@@ -33,23 +29,23 @@ function maze(){
 	ctx.fillStyle = "#FF0000";   
 	ctx.beginPath();
 	
-	ctx.moveTo(1/4*window.innerWidth,1/4*window.innerHeight);//first line
-	ctx.lineTo(3/4*window.innerWidth,1/4*window.innerHeight);
+	ctx.moveTo(1/4*window.innerWidth,1/4*window.innerHeight-150);//first line
+	ctx.lineTo(3/4*window.innerWidth,1/4*window.innerHeight-150);
 	
-	ctx.moveTo(1/4*window.innerWidth,2/4*window.innerHeight);//2nd horizaonral
-	ctx.lineTo(3/8*window.innerWidth,2/4*window.innerHeight);
+	ctx.moveTo(1/4*window.innerWidth,2/4*window.innerHeight-150);//2nd horizaonral
+	ctx.lineTo(1/2*window.innerWidth,2/4*window.innerHeight-150);
 	
-	ctx.moveTo(5/8*window.innerWidth,2/4*window.innerHeight);//3rd
-	ctx.lineTo(4/4*window.innerWidth,2/4*window.innerHeight);
+	ctx.moveTo(5/8*window.innerWidth,2/4*window.innerHeight-150);//3rd horizontal
+	ctx.lineTo(4/4*window.innerWidth,2/4*window.innerHeight-150);
 	
-	ctx.moveTo(1/4*window.innerWidth,1/4*window.innerHeight);//linedown left
-	ctx.lineTo(1/4*window.innerWidth,1/2*window.innerHeight);
+	ctx.moveTo(1/4*window.innerWidth,1/4*window.innerHeight-150);//linedown left
+	ctx.lineTo(1/4*window.innerWidth,1/2*window.innerHeight-150);
 	
-	ctx.moveTo(3/8*window.innerWidth,2/4*window.innerHeight);//2nd line
-	ctx.lineTo(3/8*window.innerWidth,window.innerHeight);
+	ctx.moveTo(4/8*window.innerWidth,2/4*window.innerHeight-150);//2nd line
+	ctx.lineTo(4/8*window.innerWidth,window.innerHeight-150);
 	
-	ctx.moveTo(5/8*window.innerWidth,2/4*window.innerHeight);//3rd line
-	ctx.lineTo(5/8*window.innerWidth,window.innerHeight);
+	ctx.moveTo(5/8*window.innerWidth,2/4*window.innerHeight-150);//3rd line
+	ctx.lineTo(5/8*window.innerWidth,window.innerHeight-150);
 	ctx.stroke();
 	
 }
@@ -57,4 +53,3 @@ function maze(){
 function main(){
     maze();
 }
-
